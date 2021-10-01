8ecb81f59829a76f03c7c70f2eec3ef79809c211
mattsmartey13 <matthew.victor.smart96@gmail.com>
2021-10-01 17:02:13 +0100

    main setup new project repository, started on routing

---
+++ README.md
@@ -0,0 +1,1 @@
+# an-algorithmic-art-website
---
+++ frontend/static/js/index.js
@@ -0,0 +1,43 @@
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+}
+
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("Viewing home") },
+        { path: "/about-me", view: () => console.log("Viewing about me") },
+        { path: "/artwork", view: () => console.log("Viewing artwork") },
+    ];
+
+    //test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        }
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
+
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
+
+    console.log(match.route.view());
+    //console.log(match);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
+});
